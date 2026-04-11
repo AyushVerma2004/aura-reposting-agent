58aaec7104ed98b2c3993e0b619ce76d894d6adc
Sumit Saggar <sumitsaggar964@gmail.com>
2026-04-11 15:32:00 +0530

    Logo update

--- Aura/src/App.jsx
+++ Aura/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import logo from "./assets/logo.png";
 
 const App = () => {
   return (
     <div>
-      App
+      <div>App</div>
+      <div>
+        <img src={logo} alt="Logo" />
+        <h1>My App</h1>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default App
